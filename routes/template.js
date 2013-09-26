da7076a89c6f726ba48377399dc4fe57da2f6a94
Ryan <ryan.b.robinson@gmail.com>
2013-09-26 12:48:41 -0700

    commits for demo site

---
+++ routes/template.js
@@ -0,0 +1,47 @@
+/*
+ * GET tempalte.
+ */
+
+var databaseUrl = "wato",
+	collections = ["articles"],
+	db = require("mongojs").connect(databaseUrl, collections),
+	fs = require('fs'),
+	utils = require('./../utils');
+
+exports.indexPage = function(req,res){
+	utils.getMenuFileList(function(err, render_obj){
+		res.render('auth/template', render_obj)
+	})
+}
+
+exports.file = function(req,res){
+	res.sendfile('./views/'+req.params.template_name);
+}
+
+exports.post = function(req, res){
+	if (req.session.permissions <= 1){
+		res.status(403).send('You do not have the necessary permissions to save')
+	} else {
+		fs.writeFile('./views/'+req.body.filename,req.body.content,function(err){
+			if (err) {
+				res.status(500).send('Error saving template')
+			} else {
+				res.send('Saved taemplate');
+			}
+		})
+	}
+}
+exports.preview = function(req, res){
+	if (!req.session){
+		res.status(404).render('404');
+	} else {
+		fs.writeFile('./views/__preview.jade',req.body.content,function(err){
+			if (err) {
+				res.status(500).send('Error saving template')
+			} else {
+				res.send('Preview Ready');
+			}
+		})
+	}
+}
+
