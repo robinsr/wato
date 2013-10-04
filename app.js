3da0e9eeb8ff7005b65b8cfa680635d63622d820
Ryan <ryan.b.robinson@gmail.com>
2013-10-03 23:41:28 -0700

    added CORS support for all routes

--- app.js
+++ app.js
@@ -48,7 +48,11 @@ if ('development' == app.get('env')) {
 
 // remove dev directory from request
 
-
+app.all('/*', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+});
 
 // defines routes for readers
 app.get('/', routes.index);
