28df7bc529fdb449263d240d903fc95685efd619
Ryan Robinson <ryan.b.robinson@gmail.com>
2014-09-16 17:01:08 -0700

    Updated Gruntfile; added concurrent, watch, and nodemon

--- Gruntfile.js
+++ Gruntfile.js
@@ -14,11 +14,39 @@ module.exports = function(grunt) {
         ],
         ignorePath: /\.\.\/\.\.\/client/
       }
-    }
+    },
+    watch: {
+      app: {
+        files: ['src/**/*.js'],
+        tasks: ['wiredep:app']
+      }
+    },
+    concurrent: {
+      dev: {
+        tasks: ['nodemon', 'watch'],
+        options: {
+          logConcurrentOutput: true
+        }
+      }
+    },
+    nodemon: {
+      dev: {
+        script: 'src/server/app.js',
+        options: {
+          env: {
+            NODE_ENV: 'development'
+          }
+        }
+      }
+    },
   });
 
   grunt.loadNpmTasks('grunt-wiredep');
+  grunt.loadNpmTasks("grunt-contrib-watch");
+  grunt.loadNpmTasks('grunt-nodemon');
+  grunt.loadNpmTasks('grunt-concurrent')
 
-  grunt.registerTask('default', ['wiredep:app']);
+  grunt.registerTask('default', ['wiredep:app', 'watch:app']);
+  grunt.registerTask('server:dev', ['concurrent:dev']);
 
 };
