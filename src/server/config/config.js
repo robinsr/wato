7ed697e9f25ccb1217d9cc24b019d07b844d5ef9
Ryan Robinson <ryan.b.robinson@gmail.com>
2014-09-16 17:01:29 -0700

    Updated a whole lot of config

--- src/server/config/config.js
+++ src/server/config/config.js
@@ -1,8 +1,35 @@
+
 /**
- * Module Dependencies
+ * Module dependencies.
  */
+
 var path = require('path');
+var extend = require('util')._extend;
+
+var development = require('./env/development');
+var test = require('./env/test');
+var production = require('./env/production');
+
+var notifier = {
+  service: 'postmark',
+  APN: false,
+  email: true, // true
+  actions: ['comment'],
+  tplPath: path.normalize(__dirname + '/../app/mailer/templates'),
+  key: 'POSTMARK_KEY'
+};
+
+var defaults = {
+  appRoot: path.normalize(__dirname + '/../../'),
+  notifier: notifier
+};
+
+/**
+ * Expose
+ */
 
 module.exports = {
-    appRoot: path.normalize(__dirname + "../../../")
-}
+  development: extend(development, defaults),
+  test: extend(test, defaults),
+  production: extend(production, defaults)
+}[process.env.NODE_ENV || 'development'];
--- src/server/config/express.js
+++ src/server/config/express.js
@@ -12,30 +12,16 @@ module.exports = function(app, config) {
     app.use(express.errorHandler());
   }
 
-  app.use('/components', express.static(config.appRoot + 'client/components'));
-  app.use('/stylesheets', express.static(config.appRoot + 'client/stylesheets'));
-  app.use('/bootstrap', express.static(config.appRoot + 'client/bootstrap'));
-  app.use('/fonts', express.static(config.appRoot + 'client/fonts'));
-  app.use('/images', express.static(config.appRoot + 'client/images'));
-  //TODO remove javascripts static route
-  app.use('/javascripts', express.static(config.appRoot + 'client/javascripts'));
-
-  // remove dev directory from request
-
-  app.all('/*', function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "X-Requested-With");
-    next();
-  });
+  console.log(config.appRoot);
 
   // all environments
-  app.set('port', process.env.PORT || 8126);
+  app.use(express.logger('dev'));
+  app.set('port', process.env.PORT || 11080);
   app.set('views', config.appRoot + 'server/publicviews/');
   app.set('view engine', 'jade');
   app.use(express.cookieParser('some secret'));
   app.use(express.session())
   app.use(express.favicon());
-  app.use(express.logger('dev'));
   app.use(express.bodyParser());
   app.use(express.methodOverride());
   app.locals({
@@ -44,5 +30,22 @@ module.exports = function(app, config) {
       demo_mode: true
   })
   app.use(function(req,res,next){ req.locals = app.locals;next()});
+  
+  // static files
+  app.use('/components', express.static(config.appRoot + 'client/components'));
+  app.use('/stylesheets', express.static(config.appRoot + 'client/stylesheets'));
+  app.use('/bootstrap', express.static(config.appRoot + 'client/bootstrap'));
+  app.use('/fonts', express.static(config.appRoot + 'client/fonts'));
+  app.use('/images', express.static(config.appRoot + 'client/images'));
+  //TODO remove javascripts static route
+  app.use('/javascripts', express.static(config.appRoot + 'client/javascripts'));
+
+  // CORS 
+  app.all('/*', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    next();
+  });
+
   app.use(app.router);
 }
